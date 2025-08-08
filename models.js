7819b3e87db48d17b6f239da74cdb0f6ac89b80c
Peter Sanford <psanford@sanford.io>
2025-08-08 10:50:30 -0700

    Update models list

--- models.js
+++ models.js
@@ -1,36 +1,52 @@
 const CLAUDE_MODELS = [
   {
-    id: "claude-3-7-sonnet-latest",
-    name: "Claude 3.7 Sonnet (latest)",
+    id: "claude-opus-4-1",
+    name: "Claude Opus 4.1 (Most Capable)",
+  },
+  {
+    id: "claude-opus-4-1-20250805",
+    name: "Claude Opus 4.1"
+  },
+  {
+    id: "claude-opus-4-0",
+    name: "Claude Opus 4"
+  },
+  {
+    id: "claude-opus-4-20250514",
+    name: "Claude Opus 4 (20250514)"
+  },
+  {
+    id: "claude-sonnet-4-0",
+    name: "Claude Sonnet 4",
     isDefault: true
+  },
+  {
+    id: "claude-sonnet-4-20250514",
+    name: "Claude Sonnet 4 (20250514)"
+  },
+  {
+    id: "claude-3-7-sonnet-latest",
+    name: "Claude 3.7 Sonnet (Latest)"
   },
   {
     id: "claude-3-7-sonnet-20250219",
     name: "Claude 3.7 Sonnet"
   },
   {
-    id: "claude-3-5-haiku-latest",
-    name: "Claude 3.5 Haiku (latest)"
+    id: "claude-3-5-sonnet-latest",
+    name: "Claude 3.5 Sonnet (Latest)"
   },
   {
     id: "claude-3-5-sonnet-20241022",
-    name: "Claude 3.5 Sonnet (202410)",
+    name: "Claude 3.5 Sonnet"
+  },
+  {
+    id: "claude-3-5-haiku-latest",
+    name: "Claude 3.5 Haiku (Latest)"
   },
   {
     id: "claude-3-5-haiku-20241022",
     name: "Claude 3.5 Haiku"
-  },
-  {
-    id: "claude-3-5-sonnet-20240620",
-    name: "Claude 3.5 Sonnet (202406)"
-  },
-  {
-    id: "claude-3-opus-20240229",
-    name: "Claude 3 Opus"
-  },
-  {
-    id: "claude-3-sonnet-20240229",
-    name: "Claude 3 Sonnet"
   },
   {
     id: "claude-3-haiku-20240307",
